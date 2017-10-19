2c8c55315d0605019a2378a39bc2f81d48c36a73
nayamo <naya@val.co.jp>
2017-10-19 14:49:37 +0900

    パラメータ書式を整理

--- notify_to_slack.ps1
+++ notify_to_slack.ps1
@@ -1,14 +1,33 @@
+Param(
+  [string] $message,
+  [string] $mention,
+  [string] $sender="WinSlackNotification",
+  [switch] $an_don,
+  [switch] $emergency
+)
+
 $scriptPath = $MyInvocation.MyCommand.Path
 $s_path_parent = Split-Path -Parent $scriptPath
 $content = Import-Csv "$s_path_parent\config\config.csv"
 $config = $content | Group-Object -AsHashTable -AsString -Property Key
 
+
+$text=""
+if( [String]::IsNullOrEmpty($mention) -eq $False) {
+  $text += "<@"+$mention+">"
+}
+$text+=$message
+
 $notificationPayload = @{
-    text = $args[0];
-    username = "PowerShell BOT"; 
+    text = $text;
+    username = $sender; 
     #icon_url = "https://"
 }
 
-
 $bytes = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $notificationPayload))
 Invoke-RestMethod -Uri $config.slack_incoming_webhook_url.Value -Method Post -Body $bytes
+
+# an_don
+
+
+
--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 REM powershell /c ./notify_to_slack.ps1 "”Ï"
-powershell /c ./notify_to_slack.ps1 "ˆŸ‹‰LŠG”ö"
+powershell /c ./notify_to_slack.ps1 -mention "nayamo" -message "ˆŸ‹‰LŠG”ö" -sender "TestBot"
