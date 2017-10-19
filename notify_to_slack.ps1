e697cc2cb45e0adf007d64f59f8189bbb07eca93
nayamo <naya@val.co.jp>
2017-10-19 18:17:56 +0900

    スクリプトをパイプラインを意識した形に修正

--- notify_to_slack.ps1
+++ notify_to_slack.ps1
@@ -6,50 +6,87 @@ Param(
   [switch] $emergency
 )
 
-$rc=0
- 
-#バリデーション
-if( [String]::IsNullOrEmpty($message) )
+begin
 {
-  Write-Host "-message [メッセージ] が指定されていません。"
-  $rc=1
+  function SendSlack
+  {
+    Param(
+      [string] $text,
+      [string] $sender
+    )
+
+    begin {}
+    process
+    {
+      $notificationPayload = @{
+          text = $text;
+          username = $sender; 
+          #icon_url = "https://";
+      }
+      $bytes = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $notificationPayload))
+      Invoke-RestMethod -Uri $config.slack_incoming_webhook_url.Value -Method Post -Body $bytes
+    }
+    end {
+
+    }
+  }
+
+  $rc=0 
+  $scriptPath = $MyInvocation.MyCommand.Path
+  $s_path_parent = Split-Path -Parent $scriptPath
+  $content = Import-Csv "$s_path_parent\config\config.csv"
+  $config = $content | Group-Object -AsHashTable -AsString -Property Key
+}
+
+process
+{
+  #バリデーション
+  if( [String]::IsNullOrEmpty($message) )
+  {
+    Write-Host "-message [メッセージ] が指定されていません。"
+    $rc=1
+    return
+  }
+
+  $text=""
+  if(-Not [String]::IsNullOrEmpty($mention) )
+  {
+    $text += "<@"+$mention+">"
+  }
+  $text+=$message
+
+  SendSlack -text $text -sender $sender
+
+  # $notificationPayload = @{
+  #     text = $text;
+  #     username = $sender; 
+  #     #icon_url = "https://";
+  # }
+
+  # $bytes = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $notificationPayload))
+  # Invoke-RestMethod -Uri $config.slack_incoming_webhook_url.Value -Method Post -Body $bytes
+
+  # gasco: あんどん を使って電光掲示板にメッセージを流す
+  if($andon)
+  {
+    $andon_text = "gasco: andon"
+    if($emergency) {
+      $andon_text += " 緊急"
+    }  
+    $andon_text += " "
+    $andon_text += $message
+
+    SendSlack -text $andon_text -sender $sender
+    # $notificationPayload.text = $andon_text
+    # $bytes = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $notificationPayload))
+    # Invoke-RestMethod -Uri $config.slack_incoming_webhook_url.Value -Method Post -Body $bytes
+  }
+}
+
+end
+{
   exit $rc
 }
 
-$scriptPath = $MyInvocation.MyCommand.Path
-$s_path_parent = Split-Path -Parent $scriptPath
-$content = Import-Csv "$s_path_parent\config\config.csv"
-$config = $content | Group-Object -AsHashTable -AsString -Property Key
 
 
-$text=""
-if(-Not [String]::IsNullOrEmpty($mention) )
-{
-  $text += "<@"+$mention+">"
-}
-$text+=$message
-
-$notificationPayload = @{
-    text = $text;
-    username = $sender; 
-    #icon_url = "https://";
-}
-
-$bytes = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $notificationPayload))
-Invoke-RestMethod -Uri $config.slack_incoming_webhook_url.Value -Method Post -Body $bytes
-
-# gasco: あんどん を使って電光掲示板にメッセージを流す
-if($andon)
-{
-  $andon_text = "gasco: andon"
-  if($emergency) {
-    $andon_text += " 緊急"
-  }  
-  $andon_text += " "
-  $andon_text += $message
-  $notificationPayload.text = $andon_text
-  $bytes = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $notificationPayload))
-  Invoke-RestMethod -Uri $config.slack_incoming_webhook_url.Value -Method Post -Body $bytes
-}
-
-exit $rc
